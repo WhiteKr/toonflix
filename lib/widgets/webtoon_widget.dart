57c61dd7e5b3eaaa06446c2017450ed2732b1003
WhiteKr <hanjisu2215g@gmail.com>
2023-08-27 20:18:51 +0900

    update: move into detail page on click list item

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final WebToonModel webtoon;
@@ -11,29 +12,40 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 250,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: const Offset(10, 10),
-                color: Colors.black.withOpacity(0.5),
-              )
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(webtoon: webtoon),
+            fullscreenDialog: true,
           ),
-          child: Image.network(webtoon.thumb),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          webtoon.title,
-          style: const TextStyle(fontSize: 22),
-        ),
-      ],
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                )
+              ],
+            ),
+            child: Image.network(webtoon.thumb),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            webtoon.title,
+            style: const TextStyle(fontSize: 22),
+          ),
+        ],
+      ),
     );
   }
 }
